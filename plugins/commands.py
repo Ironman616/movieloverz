c896314a40f3fbe35ba81669673f9efe34b451b4
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2024-01-14 12:47:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -106,7 +106,7 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('💰DonateUs💰, callback_data="shortlink_info"),
+                    InlineKeyboardButton('💲Donate Us', callback_data="shortlink_info"),
                     InlineKeyboardButton('⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url=CHNL_LNK)
                 ],[
                     InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
